70a2a398617589e5454819b84fa8aacf1a0829f7
yarovik1982 <yar.oleg2018@yandex.ru>
2022-07-02 11:44:38 +0300

    создание бургера и jsx

--- js/first_component.js
+++ js/first_component.js
@@ -1,3 +1,17 @@
-const domContainer = document.querySelector('#r-block');
+
+function FirstComponent(){
+   return(
+      <div className='r-block'
+         style={{width:"70%", height:"50vh", background:"rgba(0,0,0,.7)",
+         borderRadius:"20px",
+         }}
+      ></div>
+   )
+}
+
+
+
+
+const domContainer = document.querySelector('#react-app');
 const root = ReactDOM.createRoot(domContainer);
 root.render(e(FirstComponent));
--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-import Burger from './modules/Burger.js'
+// import Burger from './modules/Burger.js'
+
+document.addEventListener("click", function(event){
+   const el = event.target;
+   if(el.closest("[data-burger]")){
+      document.querySelectorAll("[data-burger]").forEach(item=>{
+         item.classList.toggle("toggle");
+      })
+   }
+}, true)
