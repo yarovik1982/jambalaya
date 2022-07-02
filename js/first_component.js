3a9d244457fda0deb37d5eb5d97a14aaa56ba033
yarovik1982 <yar.oleg2018@yandex.ru>
2022-07-02 07:44:55 +0300

    creating

---
+++ js/first_component.js
@@ -0,0 +1,3 @@
+const domContainer = document.querySelector('#r-block');
+const root = ReactDOM.createRoot(domContainer);
+root.render(e(FirstComponent));
---
+++ js/index.js
@@ -0,0 +1,1 @@
+import Burger from './modules/Burger.js'
