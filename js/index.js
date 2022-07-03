5e44daf5f8c1b1c8f51ea1843754b3aacc56c1da
yarovik1982 <yar.oleg2018@yandex.ru>
2022-07-03 17:00:36 +0300

    03.07.2022

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// import Burger from './modules/Burger.js'
+import PrintText from "./modules/PrintText.js"
 
 document.addEventListener("click", function(event){
    const el = event.target;
@@ -7,4 +7,14 @@ document.addEventListener("click", function(event){
          item.classList.toggle("toggle");
       })
    }
+   if(el.id === "start-print" || el.id === "span-start"){
+      console.log(el);
+      PrintText()
+   }
 }, true)
+
+
+// document.addEventListener("DOMContentLoaded", ()=>{
+//    const rBtn = document.querySelector('.r-btn')
+//    console.log(rBtn);
+// })
---
+++ js/modules/PrintText.js
@@ -0,0 +1,34 @@
+const PrintText = () => {
+   const textPre = document.querySelector('.sourse')
+   const text = textPre.textContent.split(';').map(item => item = item + `\n`)
+    textPre.style.display = 'none'
+    
+     let line = 0;
+      let count = 0;
+      let result = '';
+      function typeLine() {
+        let interval = setTimeout(
+          () => {
+            result += text[line][count]
+            document.querySelector('.dist').innerHTML =result +'|';
+          count++;
+          if (count >= text[line].length) {
+            count = 0;
+            line++;
+            if (line == text.length) {
+              clearTimeout(interval);
+               document.querySelector('pre').innerHTML = result;
+               // document.querySelector('code').innerHTML = result;
+              return true;
+            }
+          }
+          typeLine();
+        }, getRandomInt(getRandomInt(250*2.5)))
+      }
+      typeLine();
+    
+    function getRandomInt(max) {
+      return Math.floor(Math.random() * Math.floor(max));
+    }
+}
+export default PrintText
